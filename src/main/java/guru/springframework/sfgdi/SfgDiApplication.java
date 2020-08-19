d6e92b8ddbe7f5e1b81aa72b758b4e890c16d9f6
Juan Manuel <juan.soto@blackboard.com>
2020-08-18 20:32:42 -0500

    Base project dependency injection till profiles

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgdi;
+
+import guru.springframework.sfgdi.controllers.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greetings = myController.sayHello();
+
+		System.out.println(greetings);
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreeting());
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println("Say hello i18n " + i18nController.sayHello());
+	}
+
+}
---
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    private PrimaryGreetingService primaryGreetingService;
+
+    public MyController(PrimaryGreetingService primaryGreetingService) {
+        this.primaryGreetingService = primaryGreetingService;
+    }
+
+    public String sayHello() {
+
+        return primaryGreetingService.sayGreeting();
+    }
+}
